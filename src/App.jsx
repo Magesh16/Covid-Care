ea1cf61653c31ca78b48b9b2e663a8bb8dc31815
Magesh16 <mageshsk16@gmail.com>
2021-09-18 08:49:08 +0530

    separating stacks

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+import { ChannelListContainer, ChannelContainer, Auth } from './components';
+const apikey = "83924mre6spp";
 
+const client = StreamChat.getInstance(apikey);
 const App = () => {
     return (
         <div>
-            <h1>Covid Care</h1>
+            <div className="app__wrapper">
+                <Chat client={client} theme="team light">
+                    <ChannelListContainer />
+                    <ChannelContainer />
+                </Chat>
+            </div>
         </div>
     )
 }
