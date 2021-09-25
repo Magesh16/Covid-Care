5db7c152770bfe25181ed6a07d8d7a6c7a9b8f22
Magesh16 <mageshsk16@gmail.com>
2021-09-25 15:54:09 +0530

    Add UserList Component

--- src/components/CreateChannel.jsx
+++ src/components/CreateChannel.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { useChatContext } from 'stream-chat-react';
+import { UserList } from "./";
+import { CloseCreateChannel } from '../assets';
 
-const ChannelInner = () => {
+const ChannelNameInput = ({ ChannelName = "", setChannelName }) => {
+    const handleChange = (e) => {
+        e.preventDefault();
+        setChannelName(e.target.value);
+    }
+
     return (
-        <div>
-            CreateChannel
+        <div className="channel-name-input__wrapper">
+            <p>Name</p>
+            <input value={ChannelName} onChange={handleChange} placeholder="channel-name" />
+            <p>Add Members</p>
         </div>
     )
 }
 
-export default ChannelInner
+const ChannelInner = ({createType, setIsCreating}) => {
+    const [channelName, setChannelName] = useState('');
+    return (
+        <div>
+            <div className="create-channel__container">
+                <div className="create-channel__header">
+                    <p>{createType === "team" ? 'Create a new Channel' : 'Send a Direct Message'}</p>
+                    <CloseCreateChannel setIsCreating={setIsCreating} />
+                </div>
+                {createType === "team" && <ChannelNameInput channelName={channelName} setChannelName={setChannelName} />}
+                <UserList/>
+            </div>
+        </div>
+    )
+}
+
+export default ChannelInner;
